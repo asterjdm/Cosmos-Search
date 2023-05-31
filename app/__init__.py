34d417d7e8adbe0f1a5fa06f2f76d9d07c1ac270
JdM <julien@rmbi.ch>
2023-05-31 17:50:27 +0200

    imporve app.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
-from flask import *
+from flask import Flask, render_template, request, redirect, url_for
 from app.engines.google import google_search
 from app.engines.wiki import getWikiSummary
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
-
 
 @app.route("/search/", methods=["GET"])
 def search():
@@ -17,21 +16,24 @@ def search():
     if query:
         if not actuPage:
             actuPage = 0
-        actuPage = int(actuPage)
+        try:
+            actuPage = int(actuPage)
+        except ValueError:
+            return {"error": "Invalid page number"}
+
         if actuPage == 0:
             wiki = getWikiSummary(query)
         else:
             wiki = []
         results = google_search(query, page=actuPage)
-        if("error" in results[0].keys()) :
-            return results
-        next_url = "/search?query=%s&page=%d" % (query, actuPage+1)
+        if "error" in results[0].keys():
+            return {"error": results[0]["error"]}
+        
+        next_url = url_for('search', query=query, page=actuPage+1)
         return render_template("index.html", results=results, query=query, info=wiki, next_url=next_url, isSearch=True)
 
     else:
         return redirect(url_for('index'))
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
