cffcc614f594f0542f5ae666cc8dd56effdfa6b4
JdM <julien@rmbi.ch>
2023-05-31 17:52:23 +0200

    improve wiki

--- app/engines/wiki.py
+++ app/engines/wiki.py
@@ -4,28 +4,27 @@ import json
 from app.utils import encode_url
 
 def get_wiki_main_image(title):
-
     url = 'https://en.wikipedia.org/w/api.php'
     data = {
-        'action' :'query',
-        'format' : 'json',
-        'formatversion' : 2,
-        'prop' : 'pageimages|pageterms',
+        'action': 'query',
+        'format': 'json',
+        'formatversion': 2,
+        'prop': 'pageimages|pageterms',
         "pithumbsize": 500,
-        'titles' : encode_url(title)
+        'titles': encode_url(title)
     }
-    response = requests.get(url, data)
+    response = requests.get(url, params=data)
     json_data = json.loads(response.text)
     try:
         return json_data['query']['pages'][0]['thumbnail']['source']
-    except KeyError as e:
+    except KeyError:
         return "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/150px-Wikipedia-logo-v2.svg.png"
 
 
 def getWikiSummary(query):
     try:
         search_result = wikipedia.search(query, results=1)
-        if(len(search_result) >= 1):
+        if len(search_result) >= 1:
             search_result = search_result[0]
             page = wikipedia.page(search_result, auto_suggest=False)
             title = page.title
@@ -36,7 +35,5 @@ def getWikiSummary(query):
             return [{"title": title, "url": url, "summary": summary, "image": image}]
         else:
             return []
-    except wikipedia.exceptions.DisambiguationError as e:
-        return []
-    except wikipedia.exceptions.PageError as e:
+    except (wikipedia.exceptions.DisambiguationError, wikipedia.exceptions.PageError):
         return []
