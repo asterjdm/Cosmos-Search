19efbff55b36c3c3fa5e830ecaf3daf272869244
JdM <julien@rmbi.ch>
2023-06-13 19:01:37 +0200

    set user agent

--- engines/ddg.go
+++ engines/ddg.go
@@ -9,7 +9,15 @@ import(
 
 func search(query string) {
 	var url string = "https://duckduckgo.com/search?q=" + query
-	resp, err := http.Get(url)
+	var client = &http.Client{}
+	const USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0"
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Set("User-Agent", USER_AGENT)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := client.Do(req)
+
 	if err != nil {
 		panic(err)
 	}
