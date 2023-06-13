8f71d36075c5c0198f709f7de6fc70c354e9993f
JdM <julien@rmbi.ch>
2023-06-13 19:27:23 +0200

    create gethtml function

---
+++ web/request.go
@@ -0,0 +1,37 @@
+package web
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"log"
+)
+
+func getHtml(url string) []byte {
+	var client = &http.Client{}
+	const USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0"
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Set("User-Agent", USER_AGENT)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+        log.Fatalf("failed to fetch data: %d %s", resp.StatusCode, resp.Status)
+    }
+
+	html, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", html)
+	return html
+}
+
