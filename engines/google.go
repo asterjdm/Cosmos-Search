834a428643a296fc059fcb8b75fd13b91d481b31
JdM <julien@rmbi.ch>
2023-06-14 17:04:21 +0200

    improve and fix google search

--- engines/google.go
+++ engines/google.go
@@ -26,10 +26,10 @@ func Search(query string, page *int) ([]map[string]string, error) {
 		return results, err
 	}
 
-	doc.Find(".kvH3mc").Each(func(i int, s *goquery.Selection) {
-		title := s.Find("h3").Text()
-		link, _ := s.Find("a").Attr("href")
-		description := s.Find(".VwiC3b").Children().Last().Text()
+	doc.Find(".VwiC3b").Each(func(i int, s *goquery.Selection) {
+		title := s.Parents().First().Parents().First().Find("h3").Text()
+		link, _ := s.Parents().First().Parents().First().Find("a").Attr("href")
+		description := s.Text()
 
 		result := map[string]string{
 			"title":       title,
