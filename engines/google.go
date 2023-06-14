ff2758a638029d6ceb1d701c30fcf470469f8207
JdM <julien@rmbi.ch>
2023-06-14 15:29:35 +0200

    Add page GET arg to choose page num

--- engines/google.go
+++ engines/google.go
@@ -1,40 +1,43 @@
 package engines
 
 import (
+	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"example.com/Cosmos-Search/web"
 )
 
-func Search(query string, page int) ([]map[string]string, error) {
-	if page == nil {
-		page := 0
+func Search(query string, page *int) ([]map[string]string, error) {
+	var pageNumber int = 0
+	if page != nil {
+		pageNumber = *page
 	}
+
 	encodedQuery := web.UrlEncode(query)
-	var url string = fmt.Sprintf("https://google.com/search?q=%s&start=%i", encodedQuery, page*10)
-	var results = []map[string]string{}
+	url := fmt.Sprintf("https://google.com/search?q=%s&start=%d", encodedQuery, pageNumber*10)
+	results := []map[string]string{}
 
 	html, err := web.GetHtml(url)
 	if err != nil {
 		return results, err
 	}
+
 	doc, err := goquery.NewDocumentFromReader(html)
 	if err != nil {
 		return results, err
 	}
 
 	doc.Find(".kvH3mc").Each(func(i int, s *goquery.Selection) {
-			title := s.Find("h3").Text()
-			link, _ := s.Find("a").Attr("href")
-			description := s.Find(".VwiC3b").Children().Last().Text()
+		title := s.Find("h3").Text()
+		link, _ := s.Find("a").Attr("href")
+		description := s.Find(".VwiC3b").Children().Last().Text()
 
-			result := map[string]string{
-				"title":       title,
-				"link":        link,
-				"description": description,
-			}
-			results = append(results, result)
-		})
+		result := map[string]string{
+			"title":       title,
+			"link":        link,
+			"description": description,
+		}
+		results = append(results, result)
+	})
 
 	return results, nil
 }
-
--- main.go
+++ main.go
@@ -1,24 +1,26 @@
 package main
 
 import (
+	_"fmt"
+	"html/template"
 	"log"
 	"net/http"
-	"html/template"
+	"strconv"
+
 	"example.com/Cosmos-Search/engines"
 )
 
-func main() {	
+func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/search/", search)
 
-	// Serve les fichiers statiques depuis le répertoire "static"
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
 
-func home(w http.ResponseWriter, r *http.Request){
+func home(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("templates/home/index.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -32,12 +34,21 @@ func home(w http.ResponseWriter, r *http.Request){
 	}
 }
 
-func search(w http.ResponseWriter, r *http.Request){
+func search(w http.ResponseWriter, r *http.Request) {
 	queryParams := r.URL.Query()
 	query := queryParams.Get("q")
-	page := queryParams.Get("page")
-	
-	results, err := engines.Search(query)
+	pageString := queryParams.Get("page")
+
+	var page int
+	var err error
+	if pageString != "" {
+		page, err = strconv.Atoi(pageString)
+		if err != nil {
+			page = 0
+		}
+	}
+
+	results, err := engines.Search(query, &page)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -51,8 +62,10 @@ func search(w http.ResponseWriter, r *http.Request){
 
 	data := struct {
 		Results []map[string]string
+		Query   string
 	}{
 		Results: results,
+		Query:   query,
 	}
 
 	err = tmpl.Execute(w, data)
