42c07e294de2badc9ea19c431dc9a67d8c132c07
JdM <julien@rmbi.ch>
2023-06-14 12:58:14 +0200

    add

--- main.go
+++ main.go
@@ -1,22 +1,42 @@
 package main
 
 import (
-    "fmt"
+    _"fmt"
     _"html"
     "log"
     "net/http"
+	"html/template"
 	"example.com/Cosmos-Search/engines"
 )
 
 func main() {
-	results, err := engines.Search("golang programming language")
+	_, err := engines.Search("golang programming language")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(results)
-    http.Handle("/", http.FileServer(http.Dir("./static/home")))
+	
+    http.HandleFunc("/", home)
 
     log.Fatal(http.ListenAndServe(":5000", nil))
 
 }
 
+func home(w http.ResponseWriter, r *http.Request){
+	tmpl, err := template.ParseFiles("templates/home/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	data := struct {
+		Title string
+	}{
+		Title: "Mon titre de page",
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
