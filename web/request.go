66aaa4c86202f3427e28c15be399f4519e1c813a
JdM <julien@rmbi.ch>
2023-06-17 16:23:18 +0200

    create web/request getJson function

--- web/request.go
+++ web/request.go
@@ -6,11 +6,13 @@ import (
 	"net/http"
 	"errors"
 	"bytes"
+	"encoding/json"
 )
+
+const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
 
 func GetHtml(url string) (*bytes.Reader, error) {
 	var client = &http.Client{}
-	const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
 	req, err := http.NewRequest("GET", url, nil)
 	req.Header.Set("User-Agent", USER_AGENT)
 	if err != nil {
@@ -37,3 +39,31 @@ func GetHtml(url string) (*bytes.Reader, error) {
 	return reader, nil
 }
 
+
+func GetJson(url string){
+	req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        return nil, err
+    }
+	req.Header.Set("User-Agent", USER_AGENT)
+	res, err := http.DefaultClient.Do(req)
+    if err != nil {
+        fmt.Print(err.Error())
+    }
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+       	return nil, err
+    }
+
+	var data Data
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
