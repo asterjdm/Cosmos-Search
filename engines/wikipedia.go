d08786b6b6189c82af408864a637072267c04010
JdM <julien@rmbi.ch>
2023-06-18 11:41:37 +0200

    get page info json

--- engines/wikipedia.go
+++ engines/wikipedia.go
@@ -7,13 +7,26 @@ import (
 
 
 func GetWiki(query string) (map[string]interface{}, error) {
-	var data []interface{}
-	var url string = fmt.Sprintf("https://en.wikipedia.org/w/api.php?action=opensearch&search=%s&limit=1&namespace=0&format=json", query)
-	err := web.GetJson(url, &data)
+	var searchData []interface{}
+	var encodedQuery string = web.UrlEncode(query)
+	var searchUrl string = fmt.Sprintf("https://en.wikipedia.org/w/api.php?action=opensearch&search=%s&limit=1&namespace=0&format=json", encodedQuery)
+	err := web.GetJson(searchUrl, &searchData)
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println(data[3])
+
+	var string resultTitle = searchData[3][1]
+
+
+	var string encodedResultTitle =  web.UrlEncode(resultTitle)
+
+	var pageInfoUrl string = fmt.Sprintf("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=%s", encodedResultTitle)
+	var pageData []interface{}
+
+	err := web.GetJson(pageInfoUrl, &pageData)
+	if err != nil {
+		return nil, err
+	}
 
 
 	return nil, nil
